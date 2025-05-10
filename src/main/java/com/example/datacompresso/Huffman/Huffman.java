bcd6e81bf894d295b0ead00f74e11b64592493f1
HAISE SASAKI <154090375+HAISESASAK-I@users.noreply.github.com>
2025-05-11 00:10:26 +0500

    Create the temporary dispaly method to check outputs

--- src/main/java/com/example/datacompresso/Huffman/Huffman.java
+++ src/main/java/com/example/datacompresso/Huffman/Huffman.java
@@ -6,39 +6,47 @@ public class Huffman {
     private HashMap<Character, Integer> frequencyMap;
     private StringBuilder builder;
     private MinHeap minHeap;
-    public Huffman(){
+
+    public Huffman() {
         this.frequencyMap = new HashMap<>();
         this.minHeap = new MinHeap();
         this.builder = new StringBuilder();
     }
-    public String encodedMessage(String message){
 
-        if(message==null||message.isEmpty()){
+    public String encodedMessage(String message) {
+
+        if (message == null || message.isEmpty()) {
             System.out.println("invalid Message");
-            return  null;
+            return null;
         }
-        for(char current:message.toCharArray()){
-            if(frequencyMap.containsKey(current)){
-                frequencyMap.replace(current,frequencyMap.get(current)+1);
-            }
-            else {
-                frequencyMap.put(current,1);
+        for (char current : message.toCharArray()) {
+            if (frequencyMap.containsKey(current)) {
+                frequencyMap.replace(current, frequencyMap.get(current) + 1);
+            } else {
+                frequencyMap.put(current, 1);
             }
         }
         createNodes();
-        return  this.builder.toString();
+        return this.builder.toString();
     }
-    private  void createNodes(){
-        for(char current: frequencyMap.keySet()){
-            minHeap.insert(new Node(frequencyMap.get(current),current));
+
+    private void createNodes() {
+        for (char current : frequencyMap.keySet()) {
+            minHeap.insert(new Node(frequencyMap.get(current), current));
         }
     }
-    private  void mergeNodes(){
-        while (minHeap.getSize()>1){
+
+    private void mergeNodes() {
+        while (minHeap.getSize() > 1) {
             Node left = minHeap.poll();
             Node right = minHeap.poll();
-            Node mergedNode = new Node(left.getFrequency()+right.getFrequency(),left,right);
+            Node mergedNode = new Node(left.getFrequency() + right.getFrequency(), left, right);
             minHeap.insert(mergedNode);
         }
+
+    }
+    public void displayHeap(){
+        System.out.println(this.frequencyMap);
+        minHeap.displayHeap();
     }
 }
--- src/main/java/com/example/datacompresso/Huffman/Main.java
+++ src/main/java/com/example/datacompresso/Huffman/Main.java
@@ -2,6 +2,8 @@ package com.example.datacompresso.Huffman;
 
 public class Main {
     public static void main(String[] args) {
-
+        Huffman huffman = new Huffman();
+        huffman.encodedMessage("abbcccdddd");
+        huffman.displayHeap();
     }
 }
