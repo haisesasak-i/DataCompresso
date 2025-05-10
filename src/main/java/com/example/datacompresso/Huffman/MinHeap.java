24c99edf2052b89c1b86a9abaaa0833a9a49edef
HAISE SASAKI <154090375+HAISESASAK-I@users.noreply.github.com>
2025-05-10 23:28:09 +0500

    Create the node class for huffman

---
+++ src/main/java/com/example/datacompresso/Huffman/MinHeap.java
@@ -0,0 +1,4 @@
+package com.example.datacompresso.Huffman;
+
+public class MinHeap {
+}
---
+++ src/main/java/com/example/datacompresso/Huffman/Node.java
@@ -0,0 +1,49 @@
+package com.example.datacompresso.Huffman;
+
+public class Node {
+    private  int frequency;
+    private  char data;
+    private  Node left;
+    private  Node right;
+    public Node(int frequency, char data) {
+        this.frequency = frequency;
+        this.data = data;
+    }
+    public  Node (int frequency, Node left, Node right){
+        this.frequency = frequency;
+        this.left = left;
+        this.right = right;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+    public char getData() {
+        return data;
+    }
+
+    public void setData(char data) {
+        this.data = data;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+}
