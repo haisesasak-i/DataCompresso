ca9ba954dac07be31f85e861ffd4ad07fcf031fd
HAISE SASAKI <154090375+HAISESASAK-I@users.noreply.github.com>
2025-05-11 00:07:37 +0500

    Create the mergeNodes method in huffman

--- src/main/java/com/example/datacompresso/Huffman/Huffman.java
+++ src/main/java/com/example/datacompresso/Huffman/Huffman.java
@@ -4,12 +4,15 @@ import java.util.HashMap;
 
 public class Huffman {
     private HashMap<Character, Integer> frequencyMap;
+    private StringBuilder builder;
     private MinHeap minHeap;
     public Huffman(){
         this.frequencyMap = new HashMap<>();
         this.minHeap = new MinHeap();
+        this.builder = new StringBuilder();
     }
     public String encodedMessage(String message){
+
         if(message==null||message.isEmpty()){
             System.out.println("invalid Message");
             return  null;
@@ -23,10 +26,19 @@ public class Huffman {
             }
         }
         createNodes();
+        return  this.builder.toString();
     }
     private  void createNodes(){
         for(char current: frequencyMap.keySet()){
             minHeap.insert(new Node(frequencyMap.get(current),current));
+        }
+    }
+    private  void mergeNodes(){
+        while (minHeap.getSize()>1){
+            Node left = minHeap.poll();
+            Node right = minHeap.poll();
+            Node mergedNode = new Node(left.getFrequency()+right.getFrequency(),left,right);
+            minHeap.insert(mergedNode);
         }
     }
 }
